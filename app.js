0450d49df1f20d1254f8c9e7241bb394bd8d9ce9
Your Name <ilin96inbox@gmail.com>
2019-01-05 19:51:00 +0800

    added error controller

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const errorController = require("./controllers/error");
+
 app.set("view engine", "ejs");
 app.set("views", "views");
 
@@ -17,8 +19,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
