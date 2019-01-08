b8c187071c58f5d20efcbb3566098c540f6eac32
Your Name <ilin96inbox@gmail.com>
2019-01-08 17:21:57 +0800

    noidea

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "node",
-  password: "Vlad1996"
+const sequelize = new Sequelize("node", "root", "password", {
+  dialect: "mysql",
+  host: "localhost"
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
